eddb532d14ad7ef2943bf12877c0f1427546673b
JBrady97 <41490750+JBrady97@users.noreply.github.com>
2018-10-16 11:05:25 -0600

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,20 @@
+// P1 Assignment
+// Author: Joseph Brady
+// Date:   1/22/2018
+// Class:  CS163/CS164
+// Email:  jcbrady@rams.colostate.edu
+public class P1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java programming is great!");
+		System.out.println("Brady, Joseph");
+		System.out.println("Computer Science");
+		System.out.println(1234 * 2);
+		System.out.println("01/22/2018");
+		System.out.println("CS163/4: CS1 Java");
+		System.out.println("I am taking this class because it's required for my major and it's interesting.");
+
+	}
+
+}
